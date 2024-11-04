a14525e276d18e072113c6eec61ed4a32b42ba8e
Barbagus Sekali <barbagus42@proton.me>
2024-11-04 22:38:06 +0100

    review the comparison example

--- examples/compare_scores.rs
+++ examples/compare_scores.rs
@@ -12,121 +12,249 @@
 // You should have received a copy of the GNU General Public License along with Foobar. If not, see
 // <https://www.gnu.org/licenses/>.
 
+//! A tool to compute and display the relative correctness of our implementation with regards to the
+//! original implementation over at [scoremyline.com](https://scoremyline.com)
+//!
+//! The expected results have been manually collected from the site and organized in so called
+//! "meta" files.
+
+use approx::assert_relative_eq;
+use core::f64;
 use serde::Deserialize;
 use serde_json;
-use slmlib::{self, files::sml::SMLDoc};
+use slmlib::{self, files::sml::SMLDoc, BurdellSettings};
 use std::{collections::HashMap, fmt::Debug, fs::read_dir};
 
-#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
-#[serde(rename_all = "UPPERCASE")]
-enum Medal {
-    Platinum,
-    Gold,
-    Silver,
-    Bronze,
-}
-
-#[derive(Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
+/// "meta" file: the burdell "level" (not sure how to qualify this)
+#[derive(Deserialize, Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[serde(rename_all = "PascalCase")]
-enum Level {
+pub enum BurdellLevel {
     Pro,
     Amateur,
     Newbie,
 }
 
+/// "meta" file: the leniency level (percentage of worst points ignored)
 #[derive(Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
-struct Score {
+struct LeniencyLevel {
+    /// the percentage of points dropped (if any)
     ignore: Option<i32>,
+    /// Maximum deviation from target line in meters.
     max_deviation: f64,
-    medal: Option<Medal>,
-    levels: HashMap<Level, f64>,
+    /// A `BurdellLevel` to burdell score mapping
+    scores: HashMap<BurdellLevel, f64>,
 }
 
+/// "meta" file: the document "root" object
 #[derive(Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
-struct Meta {
-    target_line_distance: f64,
-    scores: Vec<Score>,
+struct MetaDoc {
+    /// The target line length in kilometers
+    target_line_length: f64,
+    /// The target line length in kilometers
+    scores: Vec<LeniencyLevel>,
+}
+
+struct Record {
+    mission: String,
+    distance: f64,
+    distance_diff: f64,
+    max_dev: f64,
+    max_dev_diff: f64,
+    burdell: HashMap<BurdellLevel, (f64, f64)>,
+}
+
+impl From<BurdellLevel> for BurdellSettings {
+    fn from(value: BurdellLevel) -> Self {
+        match value {
+            BurdellLevel::Pro => slmlib::LVL_PRO,
+            BurdellLevel::Amateur => slmlib::LVL_AMATEUR,
+            BurdellLevel::Newbie => slmlib::LVL_NEWBIE,
+        }
+    }
+}
+
+impl BurdellLevel {
+    fn iterate() -> impl Iterator<Item = Self> {
+        [
+            BurdellLevel::Pro,
+            BurdellLevel::Amateur,
+            BurdellLevel::Newbie,
+        ]
+        .into_iter()
+    }
 }
 
 fn main() {
-    println!(
-        "{}",
-        [
-            "mission",
-            "level",
-            "target_line_length[orig]",
-            "target_line_length[barb]",
-            "target_line_length[diff]",
-            "max_deviation[orig]",
-            "max_deviation[barb]",
-            "max_deviation[diff]",
-            "burdell_score[orig]",
-            "burdell_score[barb]",
-            "burdell_score[diff]",
-        ]
-        .join(";")
-    );
-
-    for sml_path in read_dir("fixtures")
+    let paths = read_dir("fixtures")
         .unwrap()
         .map(|e| e.unwrap())
         .filter(|e| e.metadata().unwrap().is_file())
         .map(|e| e.path())
         .filter(|p| p.extension().is_some_and(|e| e == "sml"))
-    // .filter(|p| p.file_stem().unwrap() == "geowizard-wales1a")
-    {
-        let name = sml_path.file_stem().unwrap();
+        .collect::<Vec<_>>();
+
+    let mut records: Vec<Record> = Vec::with_capacity(paths.len());
+
+    for (i, sml_path) in paths.iter().enumerate() {
+        let name = sml_path
+            .file_stem()
+            .unwrap()
+            .to_os_string()
+            .into_string()
+            .unwrap();
+
+        eprintln!("({}/{}) {}", i + 1, paths.len(), name);
+
         let scores_path = sml_path.clone().with_extension("json");
 
-        let sml =
-            serde_json::from_reader::<_, SMLDoc>(std::fs::File::open(sml_path.clone()).unwrap())
-                .unwrap()
-                .attempt;
+        let (target_line, points) = {
+            let rdr = std::fs::File::open(sml_path.clone()).unwrap();
+            let attempt = serde_json::from_reader::<_, SMLDoc>(rdr).unwrap().attempt;
 
-        let target_line = sml.target_line();
-        let points = sml.geo_points().collect::<Vec<_>>();
+            (
+                attempt.target_line(),
+                attempt.geo_points().collect::<Vec<_>>(),
+            )
+        };
 
-        let meta =
-            serde_json::from_reader::<_, Meta>(std::fs::File::open(scores_path).unwrap()).unwrap();
+        let meta_doc = {
+            let rdr = std::fs::File::open(scores_path).unwrap();
+            serde_json::from_reader::<_, MetaDoc>(rdr).unwrap()
+        };
 
-        for score in meta.scores.into_iter().filter(|s| s.ignore.is_none()) {
-            for level in [Level::Pro, Level::Amateur, Level::Newbie].into_iter() {
-                let burdell_score = score.levels[&level];
-                let conf = slmlib::Config {
-                    burdell: match level {
-                        Level::Pro => slmlib::LVL_PRO,
-                        Level::Amateur => slmlib::LVL_AMATEUR,
-                        Level::Newbie => slmlib::LVL_NEWBIE,
-                    },
-                };
-                let res = slmlib::score_my_line(conf, target_line, points.iter().cloned());
+        for score in meta_doc.scores.into_iter().filter(|s| s.ignore.is_none()) {
+            let results = BurdellLevel::iterate()
+                .map(|level| {
+                    let conf = slmlib::Config {
+                        burdell: level.into(),
+                    };
+                    (
+                        level,
+                        (slmlib::score_my_line(conf, target_line, points.iter().cloned())),
+                    )
+                })
+                .collect::<HashMap<_, _>>();
 
-                println!(
-                    "{}",
-                    [
-                        format!("{:?}", name),
-                        format!("{:?}", level),
-                        //
-                        format!("{:.2}", meta.target_line_distance),
-                        format!("{:.2}", res.target_line_length / 1000_f64),
-                        format!(
-                            "{:.2}",
-                            (meta.target_line_distance - res.target_line_length / 1000_f64).abs()
-                        ),
-                        //
-                        format!("{:.1}", score.max_deviation),
-                        format!("{:.1}", res.max_deviation),
-                        format!("{:.1}", (score.max_deviation - res.max_deviation).abs()),
-                        //
-                        format!("{:.2}", burdell_score),
-                        format!("{:.2}", res.burdell),
-                        format!("{:.2}", (burdell_score - res.burdell).abs()),
-                    ]
-                    .join(";")
-                );
-            }
+            assert_relative_eq!(
+                results[&BurdellLevel::Pro].target_line_length,
+                results[&BurdellLevel::Amateur].target_line_length,
+                epsilon = f64::EPSILON
+            );
+            assert_relative_eq!(
+                results[&BurdellLevel::Pro].target_line_length,
+                results[&BurdellLevel::Newbie].target_line_length,
+                epsilon = f64::EPSILON
+            );
+
+            assert_relative_eq!(
+                results[&BurdellLevel::Pro].max_deviation,
+                results[&BurdellLevel::Amateur].max_deviation,
+                epsilon = f64::EPSILON
+            );
+            assert_relative_eq!(
+                results[&BurdellLevel::Pro].max_deviation,
+                results[&BurdellLevel::Newbie].max_deviation,
+                epsilon = f64::EPSILON
+            );
+
+            records.push(Record {
+                mission: name.clone(),
+                distance: results[&BurdellLevel::Pro].target_line_length / 1000_f64,
+                distance_diff: (results[&BurdellLevel::Pro].target_line_length / 1000_f64
+                    - meta_doc.target_line_length),
+                max_dev: results[&BurdellLevel::Pro].max_deviation,
+                max_dev_diff: results[&BurdellLevel::Pro].max_deviation - score.max_deviation,
+                burdell: results
+                    .iter()
+                    .map(|(level, result)| {
+                        (
+                            level.clone(),
+                            (result.burdell, result.burdell - score.scores[level]),
+                        )
+                    })
+                    .collect(),
+            });
         }
+    }
+    println!("## Distances");
+    println!(
+        "| {} |",
+        [
+            "Mission           ",
+            "Distance (km) ",
+            "Distance err. ",
+            "Max. dev. (m) ",
+            "Max. dev. err.",
+        ]
+        .join(" | ")
+    );
+    println!(
+        "|{}|",
+        [
+            ":-------------------",
+            "---------------:",
+            "---------------:",
+            "---------------:",
+            "---------------:",
+        ]
+        .join("|")
+    );
+    for record in records.iter() {
+        println!(
+            "| {} |",
+            [
+                format!("{:18}", record.mission),
+                format!("{:14.2}", record.distance),
+                format!("{:14.2}", record.distance_diff),
+                format!("{:14.1}", record.max_dev),
+                format!("{:14.1}", record.max_dev_diff),
+            ]
+            .join(" | ")
+        );
+    }
+    println!("");
+    println!("## Burdell scores");
+    println!(
+        "| {} |",
+        [
+            "Mission           ",
+            "Pro         ",
+            "Pro err.    ",
+            "Amateur     ",
+            "Amateur err.",
+            "Newbie      ",
+            "Newbie err. ",
+        ]
+        .join(" | ")
+    );
+    println!(
+        "|{}|",
+        [
+            ":-------------------",
+            "-------------:",
+            "-------------:",
+            "-------------:",
+            "-------------:",
+            "-------------:",
+            "-------------:",
+        ]
+        .join("|")
+    );
+    for record in records.iter() {
+        println!(
+            "| {} |",
+            [
+                format!("{:18}", record.mission),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Pro].0),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Pro].1),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Amateur].0),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Amateur].1),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Newbie].0),
+                format!("{:12.2}", record.burdell[&BurdellLevel::Newbie].1),
+            ]
+            .join(" | ")
+        );
     }
 }
