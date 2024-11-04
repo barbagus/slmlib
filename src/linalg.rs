adfa2ac90c698688b9233ec6a7b42725ce8ff419
Barbagus Sekali <barbagus42@proton.me>
2024-11-04 22:47:16 +0100

    Fix license notices

--- src/linalg.rs
+++ src/linalg.rs
@@ -9,7 +9,7 @@
 // slmlib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 // the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 // Public License for more details.
-// You should have received a copy of the GNU General Public License along with Foobar. If not, see
+// You should have received a copy of the GNU General Public License along with slmlib. If not, see
 // <https://www.gnu.org/licenses/>.
 
 /// A 3D Vector basic implementation
